a11b9c85f7134cf4299498714fc0da64bc08da80
Michael Trindade <michael.trindade@hotmail.com>
2024-07-19 14:26:48 -0400

    Component header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
+import Header from "./components/Header";
 const App = () => {
-  return <h1>Hello Tailwind</h1>;
+  return <Header />;
 };
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <span>Olá, Usuário</span>
+      <h1>MTS Newsletter</h1>
+      <input type="checkbox" />
+    </div>
+  );
+};
+export default Header;
