fe4b9f9094bcddbf4b9e137f8a35e0e1076a9197
Michael Trindade <michael.trindade@hotmail.com>
2024-07-20 14:02:19 -0400

    Input stylized

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,13 +10,21 @@ const Form = ({ onSubmit }) => {
   return (
     <form
       onSubmit={safeSubmit}
-      className="h-full flex flex-col items-center justify-center gap-10 -mt-20"
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20  mx-5"
     >
-      <input type="text" placeholder="Insira seu nome..." />
-      <input type="email" placeholder="Insira seu e-mail..." />
+      <input
+        type="text"
+        className="py-1 px-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none"
+        placeholder="Insira seu nome..."
+      />
+      <input
+        type="email"
+        className="py-1 px-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none"
+        placeholder="Insira seu e-mail..."
+      />
       <button
         type="submit"
-        className="py-1 px-5 bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase"
+        className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase"
       >
         Seguir
       </button>
