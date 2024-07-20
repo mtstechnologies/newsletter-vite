752af169e612ce81f5be6bbd7202c779c7db295f
Michael Trindade <michael.trindade@hotmail.com>
2024-07-20 14:39:23 -0400

    creating properties for input component

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,3 +1,4 @@
+import Input from "../Input/Input";
 const Form = ({ onSubmit }) => {
   const safeSubmit = (event) => {
     event.preventDefault();
@@ -12,16 +13,8 @@ const Form = ({ onSubmit }) => {
       onSubmit={safeSubmit}
       className="h-full flex flex-col items-center justify-center gap-10 -mt-20  mx-5"
     >
-      <input
-        type="text"
-        className="py-1 px-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none"
-        placeholder="Insira seu nome..."
-      />
-      <input
-        type="email"
-        className="py-1 px-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none"
-        placeholder="Insira seu e-mail..."
-      />
+      <Input type="text" required placeholder="Insira seu nome..." />
+      <Input type="email" required placeholder="Insira seu e-mail..." />
       <button
         type="submit"
         className="py-1 px-5 w-full max-w-sm bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 uppercase"
